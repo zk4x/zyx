bb58b322e2d96f2cc06a95fb44f522ed97775137
X <X>
2022-09-28 08:00:29 +0200

    proposal to rewrite all tensor backward ops

--- src/tensor/ops.rs
+++ src/tensor/ops.rs
@@ -70,6 +70,40 @@ where
     }
 }
 
+// Maybe it will be needed to just rewrite everything in this way
+// It will make TensorFunc clonable and also allow us to have nicer
+// print output while printing TensorFunc
+/*trait Backward<S> {
+    fn backward(self, res_grad: S);
+}
+
+struct ReLUBackward<'a, S> {
+    self_grad: &'a std::cell::RefCell<S>,
+    self_data: Rc<S>,
+}
+
+impl<'a, S> Clone for ReLUBackward<'a, S> {
+    fn clone(&self) -> Self {
+        Self {
+            self_grad: self.self_grad,
+            self_data: self.self_data.clone(),
+        }
+    }
+}
+
+impl<'a, S> Backward<S> for ReLUBackward<'a, S>
+where
+    for<'b> &'b S: ops::ReLU<Output = S>
+        + ops::DReLU<Output = S>
+        + std::ops::Mul<Output = S>
+        + std::ops::Add<Output = S>,
+{
+    fn backward(self, res_grad: S) {
+        use ops::DReLU;
+        self.self_grad.replace_with(|grad| &*grad + &(&res_grad * &self.self_data.drelu()));
+    }
+}*/
+
 impl<'g, S> ops::ReLU for &'g TensorGrad<S>
 where
     S: 'g,
