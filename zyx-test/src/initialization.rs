8993f92f77499564089537b6fb6a2b7288ee2974
zk4x <zk4x@tutamail.com>
2024-05-25 11:33:30 +0200

    fixed example

---
+++ zyx-test/src/initialization.rs
@@ -0,0 +1,29 @@
+use zyx::DType;
+
+fn randn(dtype: DType) {
+    // TODO
+}
+
+fn uniform(dtype: DType) {
+    // TODO
+}
+
+fn kaiming_uniform(dtype: DType) {
+    // TODO
+}
+
+fn zeros(dtype: DType) {
+    // TODO
+}
+
+fn ones(dtype: DType) {
+    // TODO
+}
+
+fn full(dtype: DType) {
+    // TODO
+}
+
+fn eye(dtype: DType) {
+    // TODO
+}
