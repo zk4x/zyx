1e0db7e4cee6ef1c3429d5bd5e53cd1663eb60f5
zk4x <zk4x@tutamail.com>
2024-09-04 13:56:57 +0200

    movement tests template

---
+++ zyx/tests/movement.rs
@@ -0,0 +1,21 @@
+use zyx::ZyxError;
+
+#[test]
+fn reshape() -> Result<(), ZyxError> {
+    Ok(())
+}
+
+#[test]
+fn expand() -> Result<(), ZyxError> {
+    Ok(())
+}
+
+#[test]
+fn permute() -> Result<(), ZyxError> {
+    Ok(())
+}
+
+#[test]
+fn pad_zeros() -> Result<(), ZyxError> {
+    Ok(())
+}
