d891a6284a3c546ea2f188962eb9d26fc40e2eb9
x <zk4x@tutamail.com>
2026-06-30 12:21:29 +0200

    egraph: disable egraph path (back to old kernelizer)

--- zyx/src/runtime.rs
+++ zyx/src/runtime.rs
@@ -691,7 +691,7 @@ impl Runtime {
             );
         }
 
-        let use_egraph = true;
+        let use_egraph = false;
         if use_egraph {
             let inputs: Vec<TensorId> = order.iter().copied().filter(|tid| realized_nodes.contains(tid)).collect();
             self.launch_or_store_graph_with_order(&inputs, &to_eval, &order)?;
